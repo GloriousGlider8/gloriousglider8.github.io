0da928e7a564a91287fbf8b17c059559c6360fde
GloriousGlider8 <sam.yt.gg8@gmail.com>
2025-09-22 18:18:05 +0100

    start writing

---
+++ svr-test.py
@@ -0,0 +1,16 @@
+from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__)
+cwd = os.getcwd()
+
+@app.route('/<path:filename>')
+def serve_file(filename):
+	return send_from_directory(cwd, filename)
+
+@app.route("/")
+def index():
+    return send_from_directory(cwd, "index.html")
+
+if __name__ == '__main__':
+	app.run(debug=False)
